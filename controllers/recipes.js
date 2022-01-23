3ddd93018317a935184cc1a982199b51fbf374f0
wingsforsophia <shseremetis@gmail.com>
2022-01-23 11:01:33 -0500

    added comments and second func done

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -9,24 +9,28 @@ function findRecipeNames(req, res){
 }
 
 function getDetails(req, res){
+    // make new response object
     let recipeDetails = {
         'details': {
             'ingredients': [],
     }}
-
+    // set variable for name params
     let name = req.params.recipeName
-    
+    // tests name param against data set for a name match
     data.recipes.forEach(function(recipe){
         if (recipe.name === name){
+            // if true, push each ingredient into the new object
             recipe.ingredients.forEach(function(ingredient){
                 recipeDetails.details.ingredients.push(ingredient)
             })
+            // add number of steps
             recipeDetails.numSteps = recipeDetails.details.ingredients.length
+            //respond with new object
             res.status(200).json(recipeDetails) 
         } 
     })
-
-    
+    //if conditions are not met, return empty json object
+    res.status(200).json({}) 
 }
 
 module.exports = {
